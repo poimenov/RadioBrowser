2c1b10b17dd1e727b285876c9d92b61802ae0db0
poimenov <poimenov@gmail.com>
2025-08-15 18:51:40 +0300

    Optimized appsettings and project file

--- src/AppSettings.fs
+++ src/AppSettings.fs
@@ -4,7 +4,6 @@ module RadioBrowser.AppSettings
 open System
 open System.Globalization
 open System.IO
-open System.Reflection
 open System.Text.Json
 open System.Text.Json.Serialization
 open Microsoft.FluentUI.AspNetCore.Components
@@ -19,21 +18,18 @@ type public AppSettings() =
 
     static member AppDataPath =
         Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            Environment.GetFolderPath Environment.SpecialFolder.LocalApplicationData,
             AppSettings.ApplicationName
         )
-
-    static member AssemblyFolderPath =
-        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
 
     static member DataBasePath =
         Path.Combine(AppSettings.AppDataPath, AppSettings.DataBaseFileName)
 
     static member LogConfigPath =
-        Path.Combine(AppSettings.AssemblyFolderPath, AppSettings.LogConfigFileName)
+        Path.Combine(AppContext.BaseDirectory, AppSettings.LogConfigFileName)
 
     static member WwwRootFolderPath =
-        Path.Combine(AppSettings.AssemblyFolderPath, AppSettings.WwwRootFolderName)
+        Path.Combine(AppContext.BaseDirectory, AppSettings.WwwRootFolderName)
 
     member val WindowWidth: int = 1024 with get, set
     member val WindowHeight: int = 768 with get, set
